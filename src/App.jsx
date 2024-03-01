addfcff01cf073bb34125db663dc805bca84ec44
Love Efraimsson <loveefraimsson@gmail.com>
2024-03-01 12:07:16 +0100

    The questions is decoded and sent to Quizpage component, Startpage and Quizpage showing depending och gameInProgress state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
 import Startpage from './components/Startpage';
+import Quizpage from './components/Quizpage';
 
-import './App.css'
+import './App.css';
+
+import { Base64 } from 'js-base64';
 
 
 
@@ -22,38 +25,64 @@ function App() {
   
   const [selectedCategory, setSelectedCategory] = useState({
     categoryName: '',
-    id: 0,
-    questions: []
+    id: 0
   });
+
+  const [questions, setQuestions] = useState([]);
+  const [gameinProgress, setGameInProgress] = useState(false);
+
+
 
   function selectCategoryFunction(event) {
     setSelectedCategory({
       categoryName: event.target.value,
-      id: event.target.selectedOptions[0].id,
-      questions: [],
+      id: event.target.selectedOptions[0].id
     })
   }
 
   function handleStartQuizBtn() {
-    fetch(`https://opentdb.com/api.php?amount=3&category=${selectedCategory.id}&difficulty=easy&type=multiple`)
+    fetch(`https://opentdb.com/api.php?amount=3&category=${selectedCategory.id}&difficulty=easy&type=multiple&encode=base64`)
     .then(res => res.json())
     .then((data) => {  
-      console.log(data);
-      setSelectedCategory(prevState => {
-        return {
-          ...prevState,
-          questions: data.results
-        }
+      
+      setQuestions(data.results.map(item => {
+        return({
+          type: Base64.decode(item.type),
+          difficulty: Base64.decode(item.difficulty),
+          category: Base64.decode(item.category),
+          question: Base64.decode(item.question),
+          correct_answer: Base64.decode(item.correct_answer),
+          incorrect_answers: item.incorrect_answers.map((incorrectAnswer => {
+            return Base64.decode(incorrectAnswer);
+          }))
+        }) 
+        
+      }))
 
-      })
+      setGameInProgress(true)
+
+      /* console.log( (data.results.map(item => {
+        return item.questions
+      })));
+
+      console.log( (data.results.map(item => {
+        return Base64.decode(item.question)
+      }))); */
+
     })
   }
-  
+
 
   return (
     <>
       <h1>It's quiz time!</h1>
-      <Startpage selectCategoryFunction={selectCategoryFunction} handleStartQuizBtn={handleStartQuizBtn} />
+
+      {
+        gameinProgress ? <Quizpage questions={questions} />
+        : <Startpage selectCategoryFunction={selectCategoryFunction} handleStartQuizBtn={handleStartQuizBtn} />
+      }
+
+      
     </>
   )
 }
---
+++ src/components/Quizpage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Quizpage(props) {
+
+    console.log(props);
+    return(
+        <>
+            <h2>Quizpage</h2>
+        </>
+    )
+}
+
+export default Quizpage;
